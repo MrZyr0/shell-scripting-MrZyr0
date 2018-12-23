793a4406bd8edff0fd3056281eca729ddf5e4bcd
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-12-23 23:59:22 +0100

    fix colors & absolut path & mkdir & picture sort etc...

--- ultimateCleaner.sh
+++ ultimateCleaner.sh
@@ -1,67 +1,121 @@
 #!/bin/bash
 
 # COLORS DEFINITION
-BLACK="\[\033[0;30m\]"      # black
-RED="\[\033[0;31m\]"        # red
-GREEN="\[\033[0;32m\]"      # green
-YELLOW="\[\033[0;33m\]"     # yellow
-BLUE="\[\033[0;34m\]"       # blue
-MAGENTA="\[\033[0;35m\]"    # magenta
-CYAN="\[\033[0;36m\]"       # cyan
-LIGHT_GREY="\[\033[0;37m\]" # light grey
-DARK_GREY="\[\033[1;30m\]"  # dark grey
-WHITE="\[\033[1;37m\]"      # white
-COLOR_OFF="\[\033[0m\]"     # off
+RED="\033[31m"            # red
+GREEN="\033[92m"          # green (actually light_green)
+YELLOW="\033[33m"         # yellow
+LIGHT_YELLOW="\033[93m"   # light_yellow
+COLOR_OFF="\033[39m"      # Clean the color, return to default
 
 
 clear
 
-echo "  ## ULTIMATE CLEANER SCRIPT ##"
+echo -e $LIGHT_YELLOW"  #############################"$COLOR_OFF
+echo -e $LIGHT_YELLOW"  ## ULTIMATE CLEANER SCRIPT ##"$COLOR_OFF
+echo -e $LIGHT_YELLOW"  #############################"$COLOR_OFF
+echo -e "\n"
 
-echo "[0] Récupération des paramètres..."
-if [ $# != 0 ] && [ $1 != "" ] && [ -d $1 ]
+
+echo -e $YELLOW"[0] Récupération des paramètres..."$COLOR_OFF
+if [ "$#"" != 0 ] && [ "$1" != "" ] && [ -d "$1" ]
 then
-    echo $1
-    WORKING_DIR=$1
+    WORKING_DIR="$1"
 else
-    echo "[#] Aucun arguments ! Il me faut le chemin du dossier à trier"
+    echo -e $RED"Aucun arguments ! Il me faut le chemin du dossier à trier"$COLOR_OFF
     exit 1
 fi
 
-CLEAN_DIR=$WORKING_DIR/../CLEAN
+CLEAN_DIR="$WORKING_DIR/CLEAN"
 
 
-echo "[1] Création du dossier de destination..."
+echo -e $YELLOW"[1] Création des dossiers de destination..."$COLOR_OFF
 
 rm -rf $CLEAN_DIR
-mkdir -p $CLEAN_DIR/Photos
-mkdir -p $CLEAN_DIR/Vidéos
-mkdir -p $CLEAN_DIR/Documents
-mkdir -p $CLEAN_DIR/Audio
-mkdir -p $CLEAN_DIR/Inclassables
+TMP="$CLEAN_DIR/TMP"
+mkdir -p "$CLEAN_DIR/Photos" "$CLEAN_DIR/Photos/sans dates"  "$CLEAN_DIR/Vidéos" "$CLEAN_DIR/Documents" "$CLEAN_DIR/Audio" "$CLEAN_DIR/Inclassables"
+mkdir $TMP
 
-echo "[2] Récupération des fichiers..."
-FILES=`find $WORKING_DIR -type f`
+echo -en "\e[33;5m[2] Trie en cours...\e[39;0m"
+IMG=0
+DOC=0
+AUDIO=0
+VIDEO=0
+OTHER=0
 
-echo "[3] Trie en cours..."
-for f in $files
+
+find "$WORKING_DIR" -name '*' -type f | while read FILE
 do
-    if [ file $f | grep 'image data' == 0 ]
-    then
-        cp file $CLEAN_DIR/Photos
-    elif [ file $f | grep 'PDF' == 0 ]
-    then
-        cp file $CLEAN_DIR/Documents
-    elif [ file $f | grep 'MPEG' == 0 ]
-    then
-        cp file $CLEAN_DIR/Audio
-    elif [ file $f | grep 'AVI' == 0 ] || [ file | grep 'XviD' == 0]
-    then
-        cp file $CLEAN_DIR/Vidéo
-    else
-        cp file $CLEAN_DIR/Inclassables
-    fi
+
+	if file "$FILE" | grep -qE 'image | bitmap'
+	then
+		((IMG++))
+
+		echo "$FILE"
+
+		YEAR=`stat -t %Y $FILE | cut -d ' ' -f 9 | sed s/\"//g`
+
+		if [ ! -d $CLEAN_DIR/Photos/$YEAR ] && [ "$YEAR" != 1 ]
+		then
+			mkdir $CLEAN_DIR/Photos/$YEAR
+		fi
+
+		MONTH=`stat -t %m $FILE | cut -d ' ' -f 9 | sed s/\"//g`
+
+		if [ ! -d $CLEAN_DIR/Photos/$YEAR/$MONTH ] && [ "$YEAR" != 1 ]
+		then
+			mkdir $CLEAN_DIR/Photos/$YEARS/$MONTH
+		fi
+
+		if [ $YEAR != "" ] && [ $MONTH != "" ] && [ "$YEAR" != 1 ] && [ "$MONTH" != 1 ]
+		then
+			cp "$FILE" "$CLEAN_DIR/Photos/$YEAR/$MONTH"
+		else
+			cp "$FILE" "$CLEAN_DIR/Photos/sans dates"
+		fi
+
+	elif file "$FILE" | grep -q 'PDF'
+	then
+		((DOC++))
+		cp "$FILE" $CLEAN_DIR/Documents
+	elif file "$FILE" | grep -q 'MPEG'
+	then
+        	((AUDIO++))
+        	cp "$FILE" $CLEAN_DIR/Audio
+	elif file "$FILE" | grep -q 'AVI'
+	then
+        	((VIDEO++))
+        	cp "$FILE" $CLEAN_DIR/Vidéo
+	else
+	        ((OTHER++))
+        	cp "$FILE" $CLEAN_DIR/Inclassables
+	fi
+
+echo $IMG > $TMP/IMG
+echo $DOC > $TMP/DOC
+echo $AUDIO > $TMP/AUDIO
+echo $VIDEO > $TMP/VIDEO
+echo $OTHER > $TMP/OTHER
+
 done
-echo $CLEAN_DIR
-echo "[FIN] Tri terminé"
+
+echo
+echo -e $YELLOW"[FIN]$COLOR_OFF$GREEN Tri terminé ! "$COLOR_OFF
+
+IMG=`cat $TMP/IMG`
+DOC=`cat $TMP/DOC`
+AUDIO=`cat $TMP/AUDIO`
+VIDEO=`cat $TMP/VIDEO`
+OTHER=`cat $TMP/OTHER`
+TOTAL=$(( $IMG + $DOC + $AUDIO + $VIDEO + $OTHER ))
+
+echo -e "\n"
+echo -e "$TOTAL fichier(s) au total :"
+echo -e "\t$IMG image(s)"
+echo -e "\t$DOC document(s)"
+echo -e "\t$AUDIO fichier(s) audio"
+echo -e "\t$VIDEO fichier(s) vidéo"
+echo -e "\t$OTHER fichier(s) inclassables"
+
+rm -rf $TMP
 exit 0
+
